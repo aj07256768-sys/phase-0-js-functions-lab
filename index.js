7e59bb37af85d5e9f9547c693e0c495acc971e92
aj07256768-sys <aj07256768@gmail.com>
2026-04-14 12:36:44 +0300

    completed

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 
-
-
-
-
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+  
+   // calculate tax 
+
+  function   calculateTax(amount){
+let tax =amount*0.10
+
+return tax;
+}
+
+   console.log(calculateTax(400))
+
+   // calculate to upper case 
+    
+
+   function convertToUpperCase(text){
+     let name='kaali'
+    return text.toUpperCase();
+   }
+    console.log(convertToUpperCase('abdinasir'))
+
+
+
+    // find maximum
+     function findMaximum(num,num2){
+        if (num>num2)
+        return num;
+       else (num<num2)
+      return num2
+     }
+     console.log(findMaximum(45,67))
+
+
+ 
+     // isPalindrome
+
+     function isPalindrome(word){
+        
+     let reverseD =word.split('') .reverse('') .join('')
+      
+    return reverseD===word
+     }
+
+     console.log(isPalindrome('men'))
+
+      // calculateDiscountedPrice
+
+      function calculateDiscountedPrice(original,discount){
+        
+        discountedPrice= original-discount;
+          
+        return discountedPrice
+      }
+      console.log(calculateDiscountedPrice(400,100))
+      
+
